aa55f041b2b00ff838dfb69a677875c7dec4c13f
osggis <osggis@56bb49e5-9439-0410-85c9-fc0de41135a1>
2007-09-14 22:02:38 +0000

    Fixed the VS8 project build order; added copyright blocks; updated the registry file

--- src/osgGISProjects/Serializer.cpp
+++ src/osgGISProjects/Serializer.cpp
@@ -1,3 +1,22 @@
+/**
+ * osgGIS - GIS Library for OpenSceneGraph
+ * Copyright 2007 Glenn Waldron and Pelican Ventures, Inc.
+ * http://osggis.org
+ *
+ * osgGIS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #include <osgGISProjects/Serializer>
 
 using namespace osgGISProjects;
