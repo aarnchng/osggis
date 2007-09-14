7d04e30e5092ca7072ce17dc63bb7ef27b7fb905
osggis <osggis@56bb49e5-9439-0410-85c9-fc0de41135a1>
2007-09-14 22:02:38 +0000

    Fixed the VS8 project build order; added copyright blocks; updated the registry file

--- trunk/src/OPEN-SLN-template.bat
+++ trunk/src/OPEN-SLN-template.bat
@@ -2,5 +2,6 @@
 set _=%~dp0%
 set OSGROOT=G:\devel\osg\osg2.0.0
 set GDALROOT=G:\devel\gdal-1.4.2
-set PATH=%_%\bin;%OSGROOT%\bin;%GDALROOT%\bin;%PATH%
+set EXPATROOT=G:\devel\expat-2.0.1
+set PATH=%_%\bin;%OSGROOT%\bin;%GDALROOT%\bin;%EXPATROOT%\bin;%PATH%
 osgGIS.sln
